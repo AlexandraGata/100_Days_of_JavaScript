bfd19f88700487b3fbf957f438762fb1e015ecc6
AlexandraGata <62790800+AlexandraGata@users.noreply.github.com>
2023-03-04 16:25:58 +0200

    add random number generator

---
+++ 2_Random_Number_Generator/script.js
@@ -0,0 +1,20 @@
+const number = document.querySelector(".number");
+const button = document.querySelector(".randomize");
+const minValue = 0;
+const maxValue = 10;
+
+
+button.addEventListener("click", () => {
+    number.innerHTML = getRandomInteger(0,10);
+
+})
+
+function getRandomInteger(min, max){
+    number.innerHTML = Math.floor(Math.random()*(max-min)+1);
+}
+
+button.addEventListener("click", () =>{
+   getRandomInteger(minValue, maxValue); 
+});
+
+getRandomInteger(minValue, maxValue);
